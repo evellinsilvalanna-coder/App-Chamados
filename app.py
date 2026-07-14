f77ce540497efca8006b14e6be5bff4391321e3d
evellinsilvalanna-coder <evellin.silva.lanna@gmail.com>
2026-07-14 15:10:33 -0300

    Ajuste para deploy: app.py

--- app.py
+++ app.py
@@ -1499,4 +1499,5 @@ def seed_data():
     db.session.commit()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
